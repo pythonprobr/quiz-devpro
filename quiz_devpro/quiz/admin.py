06ff4117c01698fb5fcb5c81df004e98ad3037a3
renzon <renzon@gmail.com>
2021-01-07 20:14:54 -0300

    Criadas ações para disponibilizar questões em massa

--- quiz_devpro/quiz/admin.py
+++ quiz_devpro/quiz/admin.py
@@ -4,9 +4,24 @@ from django.contrib import admin
 from quiz_devpro.quiz.models import Pergunta, Aluno, Resposta
 
 
+def tornar_disponivel(modeladmin, request, queryset):
+    queryset.update(disponivel=True)
+
+
+tornar_disponivel.short_description = "Tornar Disponivel"
+
+
+def tornar_indisponivel(modeladmin, request, queryset):
+    queryset.update(disponivel=False)
+
+
+tornar_indisponivel.short_description = "Tornar Indisponivel"
+
+
 @admin.register(Pergunta)
 class PerguntaAdmin(admin.ModelAdmin):
     list_display = ('id', 'enunciado', 'disponivel')
+    actions = [tornar_disponivel, tornar_indisponivel]
 
 
 @admin.register(Aluno)
